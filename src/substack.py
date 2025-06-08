2a9f264db062436fafabad2ddae4c86acb3f0602
DoiEnnoson <doiennoson@gmail.com>
2025-06-08 20:36:31 +0200

    Delete src directory

--- src/substack.py
+++
@@ -1,8 +0,0 @@
-import yaml
-import os
-
-def load_substack_sources():
-    path = os.path.join(os.path.dirname(__file__), '..', 'resources', 'substack_sources.yaml')
-    with open(path, 'r', encoding='utf-8') as f:
-        return yaml.safe_load(f)
-
