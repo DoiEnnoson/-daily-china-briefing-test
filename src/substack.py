ebc3f5b695e6157ffcbe155e2c53a334e1348c12
DoiEnnoson <doiennoson@gmail.com>
2025-06-08 16:38:29 +0200

    Update substack.py

--- src/substack.py
+++ src/substack.py
@@ -1,7 +1,1 @@
-import yaml
-import os
 
-def load_substack_sources():
-    base_path = os.path.join(os.path.dirname(__file__), '..', 'resources', 'substack_sources.yaml')
-    with open(base_path, 'r', encoding='utf-8') as f:
-        return yaml.safe_load(f)
