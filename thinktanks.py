11f9b942fa9e7319c7b850d72d80f5740d513031
DoiEnnoson <doiennoson@gmail.com>
2025-12-26 11:22:43 +0100

    Update thinktanks.py

--- thinktanks.py
+++ thinktanks.py
@@ -11,7 +11,6 @@ import urllib.parse
 import re
 import logging
 import json
-import markdown  # ← NEU: Für korrekte Markdown-zu-HTML Konvertierung
 
 # Logging-Konfiguration
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
@@ -307,10 +306,9 @@ def main():
     # Suche nach E-Mails der letzten 30 Tage für bessere Testabdeckung
     articles, email_count = fetch_merics_emails(email_user, email_password, days=30)
     
-    # Briefing erstellen
+    # Briefing erstellen mit korrekten Abständen
     briefing = []
     briefing.append("## Think Tanks")
-    briefing.append("")  # Leerzeile nach Hauptüberschrift
     briefing.append("### MERICS")
     
     if articles:
@@ -318,19 +316,25 @@ def main():
     else:
         briefing.append("• Keine relevanten MERICS-Artikel gefunden.")
 
-    # Briefing mit korrekten Abständen zusammenbauen
-    markdown_lines = []
-    markdown_lines.append(briefing[0])  # ## Think Tanks
-    markdown_lines.append("")  # Leerzeile
-    markdown_lines.append(briefing[2])  # ### MERICS
-    # Artikel mit <br> für Zeilenumbruch
-    for i in range(3, len(briefing)):
-        if i > 3:  # Vor jedem Artikel außer dem ersten ein <br>
-            markdown_lines.append("<br>")
-        markdown_lines.append(briefing[i])
-
-    markdown_text = "\n".join(markdown_lines)
-    html_content = markdown.markdown(markdown_text)
+    # Konvertiere zu HTML für E-Mail
+    # Zuerst Markdown-Links zu HTML konvertieren
+    html_lines = []
+    for line in briefing:
+        # Konvertiere Markdown-Links zu HTML
+        html_line = re.sub(r'\[([^\]]+)\]\(([^\)]+)\)', r'<a href="\2">\1</a>', line)
+        html_lines.append(html_line)
+    
+    # Jetzt mit korrekten Abständen zusammenbauen:
+    # - Nach "## Think Tanks": 1 Absatz
+    # - Nach "### MERICS": KEIN Absatz (direkt die Artikel)
+    # - Zwischen Artikeln: KEIN Absatz
+    html_content = html_lines[0] + "<br><br>\n"  # ## Think Tanks
+    html_content += html_lines[1] + "<br>\n"  # ### MERICS (nur 1x <br>)
+    # Restliche Zeilen (Artikel) mit einfachem <br>
+    for i in range(2, len(html_lines)):
+        html_content += html_lines[i]
+        if i < len(html_lines) - 1:  # Nicht nach dem letzten Artikel
+            html_content += "<br>\n"
     
     # E-Mail senden
     send_email("Think Tanks - MERICS Update", html_content, email_user, email_password)
